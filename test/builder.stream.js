47c3cc3055aa6c3f8376e0ab2adf80067f1a3032
itozyun <itozyun@gmail.com>
2024-10-20 05:14:37 +0900

    Added test

--- test/builder.stream.js
+++ test/builder.stream.js
@@ -44,3 +44,24 @@ test('builder.stream - 11', async t => {
 
     t.deepEqual(chunks.join(''), '[{"URL":"/","CREATED_AT":1726278571695,"MODIFIED_AT":1728347629480,"apple":1,"TOC":[1,"intro"],"pen":2,"pinapple":3},11,["P","Hello, World!"]]');
 });
+
+test('builder.stream - null false', async t => {
+    const chunks = [];
+
+    const nicePageBuilder = NicePageBuilder(
+        {
+            'templetes' : {},
+            'mixins': {"/mixin.json":[{"MIXINS":["mixin.pen.json"],"apple":false,"TOC":[1,"intro"]},1726221888590,1729081829932],"/mixin.pen.json":[{"MIXINS":["mixin.pineapple.json"],"pen":null},1729081817854,1729082148282],"/mixin.pineapple.json":[{"pinapple":0},1729081860777,1729081886161]}
+        }
+    );
+
+    await new Promise((resolve) => {
+        fs.createReadStream(__dirname + '/builder.html.json')
+          .pipe(nicePageBuilder.builder.stream())
+          // .pipe(nicePageBuilder.json2html.stream(NicePageBuilder.onInstruction[1]))
+          .on('data', (chunk)=> {chunks.push(chunk)})
+          .on('end', ()=> {resolve(chunks.join(''))});
+    });
+
+    t.deepEqual(chunks.join(''), '[{"URL":"/","CREATED_AT":1726278571695,"MODIFIED_AT":1728347629480,"apple":false,"TOC":[1,"intro"],"pen":null,"pinapple":0},11,["P","Hello, World!"]]');
+});
