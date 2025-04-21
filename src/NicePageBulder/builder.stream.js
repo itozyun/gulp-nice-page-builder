2d312f50f26a49bf2c29e2ca4b034ba0b343b3ab
itozyun <itozyun@gmail.com>
2025-04-21 15:27:06 +0900

    Update submodule

--- src/NicePageBulder/builder.stream.js
+++ src/NicePageBulder/builder.stream.js
@@ -18,7 +18,7 @@ goog.require( 'NicePageBuilder.util.completePage' );
  */
 __NicePageBuilder_internal__.builderStream = function(){
     const parser  = new JsonParser();
-    const through  = /** @type {!Through} */ (new Through( HTMLJsonParser.writeHandler, endHandler, 'builder' ));
+    const through = /** @type {!Through} */ (new Through( HTMLJsonParser.writeHandler, endHandler, 'builder' ));
 
     /**
      * @suppress {checkTypes}
